c0ef49789e6371e31c9289d054128b5a67bff9c2
kmkkiii <rskkmkmk@gmail.com>
2024-08-04 23:57:30 +0900

    add react

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
@@ -8,5 +9,6 @@ export default defineConfig({
     tailwind({
       applyBaseStyles: false,
     }),
+    react(),
   ],
 });
