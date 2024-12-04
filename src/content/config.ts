d5fb9da34e8527bbe0422870c37572872a283a93
kmkkiii <rskkmkmk@gmail.com>
2024-12-04 21:07:39 +0900

    fix legacy Collection API

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,7 +1,8 @@
 import { defineCollection, z } from 'astro:content';
+import { glob } from 'astro/loaders';
 
 const tilCollection = defineCollection({
-  type: 'content',
+  loader: glob({ pattern: '**/*.md', base: './til' }),
   schema: z.object({
     title: z.string(),
     tags: z.array(z.string()),
