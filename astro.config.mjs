bf3307834f307921e7f07a238131d69f25f41734
kmkkiii <rskkmkmk@gmail.com>
2025-01-06 08:21:11 +0900

    add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,13 @@ import tailwind from '@astrojs/tailwind';
 
 import react from '@astrojs/react';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    tailwind({
-      applyBaseStyles: false,
-    }),
-    react(),
-  ],
+  integrations: [tailwind({
+    applyBaseStyles: false,
+  }), react(), sitemap()],
   site: 'https://kmkkiii.github.io',
   base: '/til',
   image: {
